8afc54ea2fb15a433a63c47b38cdcee77c607a9b
Karen Dayanna Quemba Cruz <dayanna2034@gmail.com>
2026-02-24 13:43:44 -0500

    función de getProducts()

---
+++ main.js
@@ -0,0 +1,16 @@
+function getProducts() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const products = [
+                { id: 101, name: "mAcBook PrO", price: 2500, inStock: true },
+                { id: 102, name: "iPhOnE 15", price: 1200, inStock: false },
+                { id: 103, name: "AiRpOds mAx", price: 500, inStock: true },
+                { id: 104, name: "iPaD AiR", price: 800, inStock: true },
+                { id: 105, name: "ApPle WaTcH", price: 400, inStock: false }
+            ];
+            
+            resolve(products);
+        }, 2000);
+    });
+}
